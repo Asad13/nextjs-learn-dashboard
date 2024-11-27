4d492a1d7fde2c21c17ddbc2bcf016588020944d
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2024-11-27 21:45:38 +0600

    Dashboard Completed

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,14 @@
+'use client';
+
+const useDebounce = (func: Function, timeout: number = 300) => {
+  let timer: NodeJS.Timeout;
+
+  return (...args: any[]) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
+
+export default useDebounce;
